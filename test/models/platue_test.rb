d5394f71df3959a63b5a1123ec60f242582b61e8
Harikrishnan N <harikrishnansr92@live.com>
2018-03-03 22:00:32 +0530

    test for models

--- test/models/platue_test.rb
+++ test/models/platue_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PlatueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid width less than 1' do
+    assert_not platues(:invalid1).valid?
+  end
+
+  test 'invalid height less than 1' do
+    assert_not platues(:invalid2).valid?
+  end
 end
--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'face should be present' do
+    assert_not states(:invalid1).valid?
+  end
+
+  test 'x should not be less than 0' do
+    plateu = platues(:valid)
+    state = State.new(x: -1, y: 0, face: 'N', platue_id: plateu.id)
+    assert_not state.save
+  end
+
+  test 'y should not be less than 0' do
+    plateu = platues(:valid)
+    state = State.new(x: 0, y: -1, face: 'N', platue_id: plateu.id)
+    assert_not state.save
+  end
+
+  test 'should be valid' do
+    plateu = platues(:valid)
+    assert State.new(x: 1, y: 1, face: 'N', platue_id: plateu.id).valid?
+  end
 end
